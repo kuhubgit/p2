0cd4c1c179c67e5112b224668e82c9309b073495
kuhubgit <dylan@yosemite.local>
2015-10-25 13:18:22 +0900

    clean up

--- app/models/Helo.scala
+++ app/models/Helo.scala
@@ -4,10 +4,6 @@ import play.api.libs.json.{Reads, JsPath}
 import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 
-/**
- *
- * @author forrestinc
- */
 case class Helo(message: String, version: String)
 
 
